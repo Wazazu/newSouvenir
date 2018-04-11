dd3d1c7f4f748dc94fe4f419f26688e4b12e4ff5
Jean <jean.arhancetebehere@gmail.com>
2018-04-11 16:20:02 +0200

    add memory on map

--- src/main/webapp/app/entities/map/map.component.ts
+++ src/main/webapp/app/entities/map/map.component.ts
@@ -83,6 +83,7 @@ export class MapComponent implements OnInit, OnDestroy {
     this.markers.push({
       lat: $event.coords.lat,
       lng: $event.coords.lng,
+      label:'label',
       draggable: true,
       titre:'titre'+ this.markers.length,
       description:'description'+ this.markers.length
